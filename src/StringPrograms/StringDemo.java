2cc29fd989c639c3032794e5c604bc7cfae64022
agautam-QA <agautam1290@gmail.com>
2022-04-05 09:20:00 -0500

    Eclipse to Git Practice

---
+++ src/StringPrograms/StringDemo.java
@@ -0,0 +1,17 @@
+package StringPrograms;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s= "Java is a simple language";
+		String s1=" #C is not simple";
+		String s3=new String("Java is simple language");
+		
+		System.out.println(s);
+		System.out.println(s.trim());
+
+	}
+
+}
